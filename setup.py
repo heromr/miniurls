b267b2528476749e90f482f83b5bd11dfe8962be
heromr <84679204+heromr@users.noreply.github.com>
2022-08-13 16:11:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='short',
+    name='miniurl',
     version='0.0.1',
     license='MIT',
     author="Hero",
-    packages=find_packages('source'),
-    package_dir={'': 'source'},
-    url='https://github.com/heromr/mini-url',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/heromr/miniurl',
     keywords='shortener',
     install_requires=[
           'requests',
